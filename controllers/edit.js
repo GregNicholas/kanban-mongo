2f8916fc3a0b89324ec5ca5348f49d7615b633f4
Greg Schoenberg <gregschoenberg@gmail.com>
2023-01-23 23:49:11 -0800

    Edit boards name and column names, route, controller

--- controllers/edit.js
+++ controllers/edit.js
@@ -1,4 +1,5 @@
 const Items = require("../models/Item")
+const Boards = require("../models/Board")
 
 module.exports = {
     getEdit: async (req, res) => {
@@ -6,6 +7,16 @@ module.exports = {
         try {
             const itemToEdit = await Items.findById(id)
             res.render("edit.ejs", {item: itemToEdit, itemId: id})
+        }catch(err) {
+            if(err) return res.status(500).send(err)
+            res.redirect("/")
+        }
+    },
+    getEditBoard: async (req, res) => {
+        const id = req.params.id;
+        try {
+            const boardToEdit = await Boards.findById(id)
+            res.render("edit.ejs", {board: boardToEdit, boardId: id})
         }catch(err) {
             if(err) return res.status(500).send(err)
             res.redirect("/")
@@ -26,6 +37,23 @@ module.exports = {
         const id = req.params.id
         try{
             await Items.findByIdAndUpdate(id, req.body)
+            res.redirect("/")
+        }catch(err){
+            if(err) return res.status(500).send(err)
+            res.redirect("/")
+        }
+    },
+    updateBoard: async (req, res) => {
+        console.log(req.body)
+        const id = req.params.id
+        const boardToEdit = await Boards.findById(id)
+    // const newBoard = { name: req.body.boardName, columns: [{name: req.body.columns[0]}, {name: req.body.columns[1]}] }
+        boardToEdit.name = req.body.boardName;
+        boardToEdit.columns[0].name = req.body.columns[0]
+        boardToEdit.columns[1].name = req.body.columns[1]
+
+        try{
+            await Boards.findByIdAndUpdate(id, boardToEdit)
             res.redirect("/")
         }catch(err){
             if(err) return res.status(500).send(err)
--- routes/edit.js
+++ routes/edit.js
@@ -5,6 +5,7 @@ const editController = require('../controllers/edit')
 router.get('/:id', editController.getEdit) // get edit page with item id that we want to edit
 router.get('/delete/:id', editController.deleteItem) // delete item
 router.post('/update/:id', editController.updateItem) // update the item using data from the form
+router.get('/board/:id', editController.getEditBoard)
 // router.get('/deleteBoard/:id', editController.deleteBoard) // delete board
-// router.post('/updateBoard/:id', editController.updateBoard)
+router.post('/updateBoard/:id', editController.updateBoard)
 module.exports = router
