351de73915844f0ee6eaff258d64988cd62629e4
Greg Schoenberg <gregschoenberg@gmail.com>
2023-01-24 20:50:35 -0800

    cleanup

--- controllers/edit.js
+++ controllers/edit.js
@@ -2,16 +2,6 @@ const Items = require("../models/Item")
 const Boards = require("../models/Board")
 
 module.exports = {
-    getEdit: async (req, res) => {
-        const id = req.params.id;
-        try {
-            const itemToEdit = await Items.findById(id)
-            res.render("edit.ejs", {item: itemToEdit, itemId: id})
-        }catch(err) {
-            if(err) return res.status(500).send(err)
-            res.redirect("/")
-        }
-    },
     getEditBoard: async (req, res) => {
         const id = req.params.id;
         try {
@@ -22,31 +12,10 @@ module.exports = {
             res.redirect("/")
         }
     },
-    deleteItem: async (req, res) => {
-        const id = req.params.id
-        try{
-            await Items.findByIdAndDelete(id)
-            res.redirect("back")
-        }catch(err){
-            if(err) return res.status(500).send(err)
-            res.redirect("/")
-        }
-    },
     deleteBoard: async (req, res) => {
         const id = req.params.id
         try{
             await Boards.findByIdAndDelete(id)
-            res.redirect("back")
-        }catch(err){
-            if(err) return res.status(500).send(err)
-            res.redirect("/")
-        }
-    },
-    updateItem: async (req, res) => {
-        console.log(req.body)
-        const id = req.params.id
-        try{
-            await Items.findByIdAndUpdate(id, req.body)
             res.redirect("/")
         }catch(err){
             if(err) return res.status(500).send(err)
@@ -54,10 +23,8 @@ module.exports = {
         }
     },
     updateBoard: async (req, res) => {
-        console.log(req.body)
         const id = req.params.id
         const boardToEdit = await Boards.findById(id)
-    // const newBoard = { name: req.body.boardName, columns: [{name: req.body.columns[0]}, {name: req.body.columns[1]}] }
         boardToEdit.name = req.body.boardName;
         boardToEdit.columns[0].name = req.body.columns[0]
         boardToEdit.columns[1].name = req.body.columns[1]
--- routes/home.js
+++ routes/home.js
@@ -6,7 +6,6 @@ const router = express.Router()
 const homeController = require('../controllers/home')
 
 router.get('/', homeController.getIndex) //readonly route
-router.post('/new', homeController.createItem) //create route
 router.post('/addBoard', homeController.createBoard) // create board
 router.get('/board/:id', homeController.getBoard) // get board
 
